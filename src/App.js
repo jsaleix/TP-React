a6466db311686efe5629b6e0c3a0f0338b675b41
jsaleix <julian.saleix@gmail.com>
2021-01-02 17:52:36 +0100

    Route component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import Home from './pages/Home/Home.js';
-import './components/Header/Header.js';
+import Route from './components/Route/Route.js';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
+
+import Home from './pages/Home/Home.js';
 
 function App() {
   return (
     <>
       <Header/>
-      <Home/>
+      <Route path="/">
+        <Home/>
+      </Route>
+      <Route path="/pokedex">
+        <p>Test</p>
+      </Route>
+      <Route path="/search">
+        <p>Test</p>
+      </Route>
+      <Route path="/pokeNews">
+        <p>Test</p>
+      </Route>
       <Footer/>
     </>
   );
